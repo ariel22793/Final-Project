bf878a3138cf0a6db88b6dc42fb5b7122eb0ba93
Ariel <ariel22793@gmail.com>
2020-04-05 14:20:06 +0300

    "Change name feture fix"

--- temp.py
+++ temp.py
@@ -1248,12 +1248,12 @@ def Treeview_right_click(event):
 
     if '.png' in tree.item(item)["text"]:
         menu = Menu(explorerFrame, tearoff=0)
-        menu.add_command(label="Change Name", command = lambda: changeName(item_iid,tree.item(item)["text"]))
+        menu.add_command(label="Change Name", command = lambda: changeName(item_iid,tree.item(item)["text"], path))
         menu.add_command(label="Preview", command = lambda: image_preview(path))
         menu.add_command(label="Delete Photo")
         menu.post(event.x_root, event.y_root)
 
-def changeName(iid, fileName):
+def changeName(iid, fileName, path):
     bbox = tree.bbox(iid)
     x = bbox[0]
     y = bbox[1]
@@ -1262,12 +1262,16 @@ def changeName(iid, fileName):
     entry.insert(END, fileName)
     entry.place(x=x+30,y=y)
     entry.focus()
-    entry.bind('<FocusOut>', lambda event: out(entry, iid))
+    entry.bind('<FocusOut>', lambda event: out(entry, iid, path))
+    entry.bind('<Return>', lambda event: out(entry, iid, path))
 
-def out(entry, iid):
+def out(entry, iid, path):
     new_name = entry.get()
     entry.destroy()
     tree.item(iid, text=new_name)
+    new_path = path[0:path.rfind('/')]+'/' + new_name
+    print(new_path)
+    os.rename(path, new_path)
 
 def image_preview(filePath):
     r = Toplevel()
